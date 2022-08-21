df752905a83898f56988f2c3acacd5ccab4bc0b7
Arturo Salazar <arturo.v.salazar.jr@gmail.com>
2022-08-21 10:09:09 -0500

    Add Item class fields and functions
    Added the fields for the Item class (name and price) along with two constructors. Also set up getters and setters for these fields with checks for empty/null input for the name and negative prices for the price.

--- Main.java
+++ Main.java
@@ -1,8 +1,28 @@
+import models.Item;
+
 public class Main {
     
 
     public static void main(String[] args) {
 
+        //Test the creation of individual items
+        Item celery = new Item("Celery", 0.99);
+        System.out.print(celery);
+        Item spinach = new Item("Spinach", 0.99);
+        System.out.print(spinach);
+        Item coriander = new Item("Coriander", 1.29);
+        System.out.print(coriander);
+
+        //Test copy constructor
+        Item corianderCopy = new Item(coriander);
+        System.out.print(corianderCopy);
+
+        //Remove comment to test that exception is thrown if item name is null or blank
+        // Item emptyItem = new Item("", 1.0);
+        // Item nullItem = new Item (null, 1.0);
+
+        //Remove comment to test that exception is thrown it price is less than 0
+        // Item negativePriceItem = new Item ("Apple", -1.0);
     
     }
 
--- models/Item.java
+++ models/Item.java
@@ -1,6 +1,51 @@
 package models;
 
 public class Item {
-
+    private String name;
+    private double price;
   
+    public Item(String name, double price){
+        if (name == null || name.isBlank()){
+            throw new IllegalArgumentException("The name field cannot be blank or null");
+        }
+
+        if (price < 0){
+            throw new IllegalArgumentException("The price cannot be negative");
+        }
+
+        this.name = name;
+        this.price = price;
+    }
+
+    public Item (Item source){
+        this.name = source.name;
+        this.price = source.price;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public double getPrice(){
+        return this.price;
+    }
+
+    public void setName(String name){
+        if (name == null || name.isBlank()){
+            throw new IllegalArgumentException("The name field cannot be blank or null");
+        }
+        this.name = name;
+    }
+
+    public void setPrice(double price){
+        if (price < 0){
+            throw new IllegalArgumentException("The price cannot be negative");
+        }
+        this.price = price;
+    }
+
+    @Override
+    public String toString(){
+        return this.name + ": $" + this.price + " ";
+    }
 }
