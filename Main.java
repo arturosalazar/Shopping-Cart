a4631b9ce31d924aa095d4de9abfd0672e592534
Arturo Salazar <arturo.v.salazar.jr@gmail.com>
2022-08-23 05:13:30 -0500

    Add cart tests to Main
    Add some code to test if the cart will ignore duplicate items (ie, make sure that our compare() method uses the updated equals() method on Item class that compares values instead of references

--- Main.java
+++ Main.java
@@ -1,29 +1,20 @@
 import models.Item;
+import models.Cart;
 
 public class Main {
     
 
     public static void main(String[] args) {
 
-        //Test the creation of individual items
-        Item celery = new Item("Celery", 0.99);
-        System.out.print(celery);
-        Item spinach = new Item("Spinach", 0.99);
-        System.out.print(spinach);
-        Item coriander = new Item("Coriander", 1.29);
-        System.out.print(coriander);
+        //Test the creation of a cart with adding items
+        Cart cart = new Cart();
 
-        //Test copy constructor
-        Item corianderCopy = new Item(coriander);
-        System.out.print(corianderCopy);
-
-        //Remove comment to test that exception is thrown if item name is null or blank
-        // Item emptyItem = new Item("", 1.0);
-        // Item nullItem = new Item (null, 1.0);
-
-        //Remove comment to test that exception is thrown it price is less than 0
-        // Item negativePriceItem = new Item ("Apple", -1.0);
-    
+        cart.add(new Item("Celery", 0.99));
+        cart.add(new Item("Celery", 0.99));
+        cart.add(new Item("Spinach", 0.99));
+        cart.add(new Item("Coriander", 1.29));
+        
+        System.out.println(cart);
     }
 
     /**
