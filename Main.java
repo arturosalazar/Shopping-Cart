814c648e46f6dd45d751ed24bff7945458fb1274
Arturo Salazar <arturo.v.salazar.jr@gmail.com>
2022-08-23 22:06:23 -0500

    Add loadFile() function
    Removed previous code and added a loadFile function to load/fill the Store object from an associated file.

--- Main.java
+++ Main.java
@@ -1,31 +1,30 @@
 import models.Item;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 import models.Cart;
 import models.Store;
 
 public class Main {
+    static Store store = new Store();
+
+    static Cart cart = new Cart();
     public static void main(String[] args){
-        Item[][] inventory = new Item[][] { 
-            { new Item("Pepsi", 1.99), new Item("Crush", 1.99), new Item("Cola", 1.99) },
-            { new Item("Honey Oats", 3.99), new Item("Fruit Loops", 1.99), new Item("Cheerios", 2.99) },
-            { new Item("Milk", 4.99), new Item("Eggs", 0.99), new Item("Cheese", 1.89) },
-            { new Item("Pepperoni", 2.99), new Item("Salami", 4.49), new Item("Mortadella", 4.99) },
-            { new Item("Celery", 0.99), new Item("Spinach", 0.99), new Item("Coriander", 1.29) },
-            { new Item("Shirt", 12.99), new Item("Pants", 24.99), new Item("Sweater", 18.99) }, 
-            { new Item("Phone", 549.99), new Item("Printer", 349.99), new Item("Television", 1099) } 
-        };
-    
-        Store store = new Store();
-    
-        for (int i = 0; i < inventory.length; i++){
-            for (int j = 0; j < inventory[i].length; j++){
-                store.setItem(i, j, inventory[i][j]);
-            }
+
+        try {
+            loadItems("products.txt");
+        } catch (FileNotFoundException e) {
+            e.getMessage();
+        } finally {
+            System.out.println("\n\t******************************JAVA GROCERS******************************\n");
+            System.out.println(store);
         }
-
-        System.out.println("\n\t******************************JAVA GROCERS******************************\n");
-
-        System.out.println(store + "\n");
-    
+        
+        
+        
     }
     
     /**
@@ -55,5 +54,25 @@ public class Main {
      *      • Parse each price into a Double.
      *   2. adds all items to the store object's items field.
      */
+
+    static public void loadItems(String fileName) throws FileNotFoundException {
+        File file = new File(fileName);
+
+        Scanner scanFile = new Scanner (file);
+
+        
+        for (int i = 0; scanFile.hasNextLine(); i++){
+            String scannedLine = scanFile.nextLine();
+            String[] splitLine = scannedLine.split(";");
+            for (int j = 0; j < splitLine.length; j++) {
+                String[] splitPhrase = splitLine[j].split("=");
+                String itemName = splitPhrase[0];
+                double itemPrice = Double.parseDouble(splitPhrase[1]);
+                store.setItem(i, j, new Item(itemName, itemPrice)); 
+            }
+        }
+        
+        scanFile.close();
+    }
 
 }
